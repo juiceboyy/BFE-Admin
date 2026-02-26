59590394a6cabc823b7b4f66409635a2cc1ea2be
“Ro <halfhide@gmail.com>
2026-02-26 17:40:56 +0100

    Conclude Refactor

---
+++ js/main.js
@@ -0,0 +1,16 @@
+import { initAuth } from './api/auth.js';
+import { fetchFacturenUitGmail } from './api/gmail.js';
+import { renderFactuurSuggesties, setInboxPeriode } from './ui/inbox.js';
+import { initBtwModule } from './ui/btw.js';
+
+// Initialiseer UI componenten
+lucide.createIcons();
+setInboxPeriode();
+initBtwModule();
+
+// Initialiseer Authenticatie en data fetching
+// De callback wordt uitgevoerd zodra de gebruiker succesvol is ingelogd
+initAuth(async () => {
+    const facturen = await fetchFacturenUitGmail();
+    renderFactuurSuggesties(facturen);
+});
